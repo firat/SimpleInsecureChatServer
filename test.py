26e515621d3229a1eb4a9d065cf40ebaa27b394e
Fırat Can Başarır <firatcanbasarir@gmail.com>
2011-04-15 18:47:58 +0300

    A lot more responsive, multi-threaded client.

---
+++ test.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+import socket, select, threading, sys
+
+def test_func():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        select.select([s], [], [], 1)
+        print "Hello"
+
+if __name__ == "__main__":
+    threading.Thread(target=test_func).start()
